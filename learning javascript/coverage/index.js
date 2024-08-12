f6a75359955aee2d6005f5e5070a4e9809584e83
johnpaul kibet <johnpaulkibet3@gmail.com>
2024-08-12 11:52:29 +0300

    MORE IMPROVEMENT

---
+++ learning javascript/coverage/index.js
@@ -0,0 +1,20 @@
+	function getcube(number){  
+	alert(number*number*number);  
+}  
+ 
+function noo(){
+    alert("i told you not to click dumb ass");
+}
+
+function greeting(name){
+    var name = "ben";
+    document.write("hello ",name);
+
+}
+function circlearea(radius){
+    document.write(22/7 * radius * radius + "cm");
+}
+
+
+
+
