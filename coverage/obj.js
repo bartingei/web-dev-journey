5ed7ad3b74d17d15404dd3be02be7e5c226d0b58
johnpaul kibet <johnpaulkibet3@gmail.com>
2024-08-22 12:11:19 +0300

    made other new programs

---
+++ coverage/obj.js
@@ -0,0 +1,14 @@
+function msg(){
+    alert("welcome to browser object model in javascript");
+}
+
+function disclaimer(){
+    var v = confirm("are you sure? ");
+    if (v == true){
+        alert("ok");
+    }
+
+    else{
+        alert("cancel");
+    }
+}
